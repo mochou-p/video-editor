191c41fbd02c0e2865295c660cbfc212a88fe7bc
mochou-p <66534926+mochou-p@users.noreply.github.com>
2025-05-22 20:14:46 +0200

    macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 // video-editor/src/main.rs
 
-fn main() {
-    println!("bye zig");
+use macroquad::prelude::*;
+
+#[macroquad::main("idk")]
+async fn main() {
+    loop {
+        draw_text("IT WORKS!", 20.0, 20.0, 30.0, WHITE);
+        next_frame().await
+    }
 }
 
