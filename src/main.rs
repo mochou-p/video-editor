bc60ff4a904538e12eab9b9ca95dd48e9a144378
mochou-p <66534926+mochou-p@users.noreply.github.com>
2025-05-25 02:59:24 +0200

    full cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,63 +3,124 @@
 use macroquad::prelude::*;
 
 
-#[macroquad::main("idk")]
+#[macroquad::main("video-editor")]
 async fn main() {
-    let m = 50.0;
-
-    let mut seek    = 0.0;
-    let mut seeking = false;
+    let mut state = State::default();
+    let     style = Style::default();
 
     loop {
         if is_key_down(KeyCode::Escape) {
             break;
         }
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        clear_background(BLACK);
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        let sw = screen_width() - 2.0 * m;
-        let sh = 75.0;
-        let sx = m;
-        let sy = screen_height() - m - sh;
 
-        let (mx, my) = mouse_position();
-        let hover = if mx >= sx && my >= sy && mx <= sx+sw && my <= sy+sh {
-            if is_mouse_button_pressed(MouseButton::Left) {
-                seeking = true;
-            }
-            true
-        } else {
-            false
-        };
-        if seeking {
-            seek = ((mx - sx) / sw).clamp(0.0, 1.0);
-        }
-        if is_mouse_button_released(MouseButton::Left) {
-            seeking = false;
-        }
+        clear_background(style.background);
 
-        draw_rectangle(sx, sy, sw, sh, if seeking { DARKBLUE } else if hover { SKYBLUE } else { BLUE });
-        if hover {
-            draw_line(mx, sy, mx, sy + sh, 3.0, BLACK);
-        }
-        draw_line(sx + sw * seek, sy, sx + sw * seek, sy + sh, 3.0, WHITE);
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        let cy = sy - m;
-        draw_line(0.0, cy, screen_width(), cy, 1.0, DARKGRAY);
+        state.width                    = screen_width();
+        state.height                   = screen_height();
+        (state.mouse_x, state.mouse_y) = mouse_position();
 
-        let vcx = screen_width() * 0.5;
-        let vcy = cy * 0.5;
+        seek_bar(&mut state, &style); 
+        video(&state, &style);
 
-        let ax = screen_width() - 2.0 * m;
-        let ay = cy - 2.0 * m;
-
-        let r  = 16.0 / 9.0;
-        let hr = 1.0 / r;
-        let vs = (ax * hr).min(ay);
-
-        draw_rectangle(vcx - vs * (r * 0.5), vcy - vs * 0.5, vs * r, vs, DARKGRAY);
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         next_frame().await;
     }
+}
+
+#[derive(Default)]
+struct State {
+    // available space
+    // x:       f32,
+    // y:       f32,
+    width:   f32,
+    height:  f32,
+    // input
+    mouse_x: f32,
+    mouse_y: f32,
+    // seeking
+    seek:    f32,
+    seeking: bool
+}
+
+// these do not change with themes or resizes
+const SEEK_BAR_HEIGHT: f32 = 70.0;
+
+struct Style {
+    // space
+    margin:          f32,
+    // colors
+    line_width:      f32,
+    background:      Color,
+    video:           Color,
+    seek_bar:        Color,
+    seek_bar_hover:  Color,
+    seek_bar_click:  Color,
+    seek_line:       Color,
+    seek_hover_line: Color
+}
+
+impl Default for Style {
+    fn default() -> Self {
+        Self {
+            margin:          20.0,
+            line_width:      3.0,
+            background:      BLACK,
+            video:           DARKGRAY,
+            seek_bar:        BLUE,
+            seek_bar_hover:  SKYBLUE,
+            seek_bar_click:  DARKBLUE,
+            seek_line:       WHITE,
+            seek_hover_line: BLACK
+        }
+    }
+}
+
+fn seek_bar(state: &mut State, style: &Style) {
+    let width  = state.width - 2.0 * style.margin;
+    let x      = style.margin;
+    let y      = state.height - style.margin - SEEK_BAR_HEIGHT;
+
+    let hover = if state.mouse_x >= x && state.mouse_y >= y && state.mouse_x <= x + width && state.mouse_y <= y + SEEK_BAR_HEIGHT {
+        if is_mouse_button_pressed(MouseButton::Left) {
+            state.seeking = true;
+        }
+
+        true
+    } else {
+        false
+    };
+
+    if state.seeking {
+        state.seek = ((state.mouse_x - x) / width).clamp(0.0, 1.0);
+    }
+
+    if is_mouse_button_released(MouseButton::Left) {
+        state.seeking = false;
+    }
+
+    draw_rectangle(
+        x, y, width, SEEK_BAR_HEIGHT,
+        if state.seeking { style.seek_bar_click } else if hover { style.seek_bar_hover } else { style.seek_bar }
+    );
+
+    if hover {
+        draw_line(state.mouse_x, y, state.mouse_x, y + SEEK_BAR_HEIGHT, style.line_width, style.seek_hover_line);
+    }
+
+    draw_line(x + width * state.seek, y, x + width * state.seek, y + SEEK_BAR_HEIGHT, style.line_width, style.seek_line);
+
+    state.height = y;
+}
+
+fn video(state: &State, style: &Style) {
+    let bottom           = state.height;
+    let center_x         = screen_width() * 0.5;
+    let center_y         = bottom * 0.5;
+    let available_width  = screen_width() - 2.0 * style.margin;
+    let available_height = bottom - 2.0 * style.margin;
+    let ratio            = 16.0 / 9.0;
+    let size             = (available_width / ratio).min(available_height);
+    let half_size        = size * 0.5;
+
+    draw_rectangle(center_x - half_size * ratio, center_y - half_size, size * ratio, size, style.video);
 }
 
